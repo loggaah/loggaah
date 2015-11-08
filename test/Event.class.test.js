a83a074ab988924aa979a7c963c1da1d346b02d9
Ravi Gairola <mallox@pyxzl.net>
2015-11-08 00:57:19 -0800

    Renamed Message to Event

---
+++ test/Event.class.test.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var expect = require('chai').expect;
+
+var Level = require("..").Level;
+var MDC = require("..").MDC;
+var Event = require('../lib/Event.class');
+
+describe("Message", () => {
+    it("should create a message", () => {
+        var mdc = new MDC();
+        var error = new Error();
+        var event = new Event(Level.info, "test", mdc, error);
+        expect(event.level).to.be.equal(Level.info);
+        expect(event.message).to.be.equal("test");
+        expect(event.mdc).to.be.deep.equal(mdc);
+        expect(event.error).to.be.deep.equal(error);
+    });
+});
